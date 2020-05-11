4c802e8249eb481fc19bb727cd7d02bef1ad334b
Michel  Zuidema <83239@b5-440-32.ict.lab.locals>
2020-05-11 12:26:19 +0200

    Initial Commit

---
+++ assets/inc/header.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container">
+        <div id="branding">
+            <h1>EK 2020 Voorspellen</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Thuis</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>----</title>
+
+		<!-- Meta Tags -->
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<header>
+		    <div class="container">
+		        <div id="branding">
+		            <h1>Stageblog</h1>
+		        </div>
+		        <nav
+		            <ul>
+		                <li><a href="index.php">Thuis</a></li>
+		                <li><a href="../logout.php">Logout</a></li>
+		            </ul>
+		        </nav>
+		    </div>
+		</header>
+	</body>
+</html>
